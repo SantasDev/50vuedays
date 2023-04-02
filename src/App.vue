b862b3c631fb241d941f15a41b9faa2a6c108c4e
Cristian Henao <krishenaosanta@hotmail.com>
2023-04-01 22:51:34 -0500

    refactor: enhance folder structure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/welcome/HelloWorld.vue'
 </script>
 
 <template>
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/expand-cards">expand cards</RouterLink>
       </nav>
     </div>
   </header>
